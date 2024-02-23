45fc8b2a732a5530ad3594f2eac37e9970a429ba
Vinayak Kamath <Vinayak.Kamath@target.com>
2024-02-23 11:15:25 -0800

    Add support for sdxl-1.0

--- include/texttoimage.py
+++ include/texttoimage.py
@@ -3,12 +3,15 @@ import os
 from include.basemodel import BaseModel
 
 
-def gen_image_from_prompt(model : BaseModel, category, object, prompt):
+def gen_image_from_prompt(model : BaseModel, prompt):
   if (model.category == "kandinsky"):
-    from include.kandinsky import gen_image as gen_image
+    from include.kandinsky import gen_image
 
   elif (model.category == "openai"):
-    from include.dalle import gen_image as gen_image
+    from include.dalle import gen_image
+  
+  elif (model.category == "sdxl"):
+    from include.sdxl import gen_image 
   
   else:
       raise ValueError("Model {model} is not supported.")
