6bbfdf1cc76cc82ee05f70588c841ddc6c59266c
Vinayak Kamath <Vinayak.Kamath@target.com>
2024-02-23 11:06:53 -0800

    code clean up

--- flashcards/gen_images.py
+++ flashcards/gen_images.py
@@ -1,7 +1,7 @@
 import os
 from include.texttoimage import gen_image_from_prompt
 from include.basemodel import BaseModel
-MAX_IMAGES = 2
+MAX_IMAGES = 3
 
 output_dir = "generated_images"
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,24 +21,26 @@ with open(namelist_file) as f:
             continue
         else:
             object = line         
-            filename = object.lower().replace(" ","_") + ".png"
-            
-            category = "general" if category == "" else category 
-            category = category.lower().replace(" ", "_")
-            image_dir = os.path.join(output_dir, category)
-            if not os.path.exists(image_dir):
-                os.makedirs(image_dir)
-            reject_dir = os.path.join(image_dir, "reject")
-            if not os.path.exists(reject_dir):
-                os.makedirs(reject_dir)
 
-            filepath = os.path.join(image_dir, filename)
-            if not os.path.exists(filepath):
-                print(category + ": " + object) 
-                prompt = object + " logo in original colors on a light background, flat image" 
-                image = gen_image_from_prompt(image_model, category, object, prompt)
-                f = open(filepath, 'wb')
-                f.write(image)
+        # Create required directories 
+        category = "general" if category == "" else category 
+        category = category.lower().replace(" ", "_")
+        image_dir = os.path.join(output_dir, category)
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+        reject_dir = os.path.join(image_dir, "reject")
+        if not os.path.exists(reject_dir):
+            os.makedirs(reject_dir)
+
+        # Generate image only if it doesn't already exist
+        filename = object.lower().replace(" ","_") + ".png"
+        filepath = os.path.join(image_dir, filename)
+        if not os.path.exists(filepath):
+            print(category + ": " + object) 
+            prompt = object + " logo in original colors on a light background, flat image" 
+            image = gen_image_from_prompt(image_model, category, object, prompt)
+            f = open(filepath, 'wb')
+            f.write(image)
 
         image_cnt += 1
         if image_cnt >= MAX_IMAGES:
--- include/texttoimage.py
+++ include/texttoimage.py
@@ -6,13 +6,11 @@ from include.basemodel import BaseModel
 def gen_image_from_prompt(model : BaseModel, category, object, prompt):
   if (model.category == "kandinsky"):
     from include.kandinsky import gen_image as gen_image
-    image = gen_image(prompt)
 
   elif (model.category == "openai"):
     from include.dalle import gen_image as gen_image
-    image = gen_image(model, prompt)
   
   else:
       raise ValueError("Model {model} is not supported.")
  
-  return image
+  return gen_image(model, prompt)
