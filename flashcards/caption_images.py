b6fb84d73bca30964d6a6bf178abb3cb528aa056
Vinayak Kamath <Vinayak.Kamath@target.com>
2024-02-25 22:19:26 -0800

    Updated caption_images

--- flashcards/caption_images.py
+++ flashcards/caption_images.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-input_dir = "generated_images/dalle-3"
+input_dir = "accepted"
 
 # Get all PNGs from the input directory, recursirvely
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -23,42 +23,39 @@ for file in all_image_files:
     filename_with_ext = os.path.basename(file)
     filename_wo_ext, ext = os.path.splitext(filename_with_ext)
     caption = filename_wo_ext.replace("_", " ").title()
+    print(filename_wo_ext)
 
     # Prepare draw/text settings
     image = Image.open(file)
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("Tahoma Bold.ttf", 60)
+    font = ImageFont.truetype("Tahoma Bold.ttf", 55)
     _, _, text_width, text_height = draw.textbbox((0, 0), caption, font)
 
     # Calculate centered position
     x = (image.width - text_width) / 2
-    y = 0.9 * image.height
+    y = 0.92 * image.height
     
-    # Define the size of the area to sample around the pixel
-    area_size = 100
-
     # Get the color of the pixels in the area around (x, y)
-    area = image.crop((x - area_size//2, y - area_size//2, x + area_size//2, y + area_size//2))
+    area_size = 1000 # Use a rectangle of 20 * 5 instead of a 10 * 10 square
+    area = image.crop((x, y, x + area_size//5, y + area_size//20))
     area_colors = np.array(area)
     # Calculate the average color of the area
     average_color = area_colors.mean(axis=(0, 1))
     # Choose a text color based on the brightness
     brightness = (average_color[0]*299 + average_color[1]*587 + average_color[2]*114) / 1000
+    #draw.rectangle((x, y, x + area_size//5, y + area_size//20))
+
     if brightness > 200:  # if the background is white or almost white
       text_color = (0, 0, 0)  # black
     elif brightness < 128:  # if the background is dark
       text_color = (255, 255, 255)  # white
     else:  # if the background is light
       text_color = (0, 0, 0)  # black
-    print(f"Using {text_color} bcoz {brightness} for {caption}")
      
-       
-    # Draw text on image
-    draw.text((x, y), caption, fill=text_color, font=font)
-
     # Save image
-    out_dir = os.path.join(file_dir, "captioned")
+    draw.text((x, y), caption, fill=text_color, font=font)
+    out_dir = os.path.join(input_dir, "captioned")
     if not os.path.isdir(out_dir):
             os.mkdir(out_dir)
-    out_file = os.path.join(file_dir, out_dir, filename_wo_ext + "_captioned" + ext)
+    out_file = os.path.join(out_dir, filename_wo_ext + "_captioned" + ext)
     image.save(out_file)
