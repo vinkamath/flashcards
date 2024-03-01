447898dca049b65c141d7093d0b0554fc0d5768c
Vinayak Kamath <Vinayak.Kamath@target.com>
2024-03-01 11:12:31 -0800

    Add support for multiple print layouts

--- flashcards/print_images.py
+++ flashcards/print_images.py
@@ -3,40 +3,46 @@ from pathlib import Path
 from PIL import Image
 import img2pdf
    
-def print_images(input_dir):
+def print_images(input_dir, layout="default"):
    out_filename = "output.pdf"
 
    png_files = []
    for path in Path(input_dir).glob('**/*.png'):
       png_files.append(path)
 
-   combined_images = []
-   for i in range(0, 8, 4):
-         images = [Image.open(x) for x in png_files[i:i+4]]
-         if images:
-               # Assuming a letter size page printed at 300dpi
-               page_width = int(8.5 * 250)
-               page_height = 11 * 250
-               img_width, img_height = images[0].size
-               x_offset = (page_width - 2 *img_width)//4
-               y_offset = (page_height - 2 *img_height)//4
-               new_img = Image.new('RGB', (page_width, page_height), 'white')
+   if layout == "default":
+      with open(out_filename,"wb") as f:
+            f.write(img2pdf.convert(png_files))
+      num_pages = len(png_files)
+   elif layout == "4x4":
+      combined_images = []
+      for i in range(0, len(png_files), 4):
+            images = [Image.open(x) for x in png_files[i:i+4]]
+            if images:
+                  # Assuming a letter size page printed at 300dpi
+                  page_width = int(8.5 * 250)
+                  page_height = 11 * 250
+                  img_width, img_height = images[0].size
+                  x_offset = (page_width - 2 *img_width)//4
+                  y_offset = (page_height - 2 *img_height)//4
+                  new_img = Image.new('RGB', (page_width, page_height), 'white')
 
-               new_img.paste(images[0], (x_offset, y_offset))  
-               new_img.paste(images[1], (page_width // 2 + x_offset, y_offset))
-               new_img.paste(images[2], (x_offset, page_height// 2 + y_offset))
-               new_img.paste(images[3], (page_width // 2 + x_offset, page_height // 2  + y_offset))
+                  new_img.paste(images[0], (x_offset, y_offset))  
+                  new_img.paste(images[1], (page_width // 2 + x_offset, y_offset))
+                  new_img.paste(images[2], (x_offset, page_height// 2 + y_offset))
+                  new_img.paste(images[3], (page_width // 2 + x_offset, page_height // 2  + y_offset))
 
+                  byte_arr = io.BytesIO()
+                  new_img.save(byte_arr, format='PNG')
+                  byte_arr = byte_arr.getvalue()
+                  combined_images.append(byte_arr)
 
-               byte_arr = io.BytesIO()
-               new_img.save(byte_arr, format='PNG')
-               byte_arr = byte_arr.getvalue()
-               combined_images.append(byte_arr)
+      with open(out_filename,"wb") as f:
+            f.write(img2pdf.convert(combined_images))
+      num_pages = len(combined_images)
+   else:
+      raise ValueError(f"Unsupported print layout: {layout}")
 
-   with open(out_filename,"wb") as f:
-         f.write(img2pdf.convert(combined_images))
-
-   num_pages = len(combined_images)
    print(f"Wrote {num_pages} pages to: {out_filename}")
 
 if __name__ == "__main__":
